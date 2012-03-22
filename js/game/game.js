24233529d73b1d17a31003f8884bd70987156b68
TyOverby <ty@pre-alpha.com>
2012-03-22 02:24:19 +0100

    got the engine working how I would like it to

--- js/game/game.js
+++ js/game/game.js
@@ -1,8 +1,4 @@
-function basicGame(canvasID){
-
-    pane = new Pane(canvasID);
-    pane.setSize(500,500);
-
+function BasicGame(){
     var p = new GameMath.Vector2f(5,5);
     var p_radius = 10;
     var v = new GameMath.Vector2f(5,10);
@@ -12,39 +8,33 @@ function basicGame(canvasID){
         console.log("starting up");
 	}
 
-	this.update = function(){
-        console.log(pane.getWidth());
+	this.update = function(deltaT){
 
         p.plusEquals(v);
         v.plusEquals(a);
-        if(p.x<=0||p.x+2*p_radius>=pane.getWidth()){
+        if(p.x<=0||p.x+2*p_radius>=pane.canvas.width){
             v.x=-v.x;
         }
-        if(p.y<=0||p.y+2*p_radius>=pane.getHeight()){
+        console.log(pane.canvas.height);
+        if(p.y<=0||p.y+2*p_radius>=pane.canvas.height){
             v.y=-v.y;
             v.y = v.y/2;
+
+            v.x = v.x * 0.9;
         }
 	}
 
 
 
-	this.render = function(){
-        pane.drawRectFill(0,0,pane.getWidth(),pane.getHeight(),"white");
-
-        pane.drawCircleFill(p.x,p.y,p_radius,"red");
-	}
-
-	this.isRunning = function(){
-		return true;
+	this.render = function(pane){
+        pane.fillStyle="red";
+        pane.fillCircle(p,p_radius);
 	}
 }
 
-function start(){
-	myGame = new basicGame("canvas");
-	game = new Engine(myGame,60);  // Start the game with an FPS of 10
-	game.run();
-}
-function stop(){
-	game.stop();
+
+function initializeEngine(){
+    game = new Engine(new BasicGame(),"canvas");
+    console.log("initialized!");
 }
 
