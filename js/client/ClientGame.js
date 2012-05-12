1860965710fede47742995c810b76d7074c11fe7
TyOverby <ty@pre-alpha.com>
2012-05-13 00:43:13 +0100

    got the client end working with the normal game

--- js/client/ClientGame.js
+++ js/client/ClientGame.js
@@ -1,31 +1,54 @@
 /*jslint browser: true*/
 /*global Engine, Game, console, Vector2f*/
 
+function Person(gender) {
+    "use strict";
+    this.gender = gender;
+}
+
+Person.prototype.sayGender = function () {
+    "use strict";
+    console.log(this);
+};
+
 function ClientGame() {
     "use strict";
     
+    console.log("CLIENT GAME");
     this.pane = -1;
-
-
+    
+    Game.call(this);
 }
 
 ClientGame.prototype = new Game();
 ClientGame.prototype.constructor = ClientGame;
 
 ClientGame.prototype.load = function () {
+    "use strict";
+    console.log("client loaded");
+    
+    Game.prototype.load.call(this);
+    
     this.pane = document.getElementById("canvas").getContext("2d");
 };
 
 ClientGame.prototype.update = function (deltaT) {
+    "use strict";
+    
+    console.log("update from ClientGame");
+    
     Game.prototype.update.call(this);
-    this.render();
+    ClientGame.prototype.render.call(this);
 };
 
 
 
 ClientGame.prototype.render = function () {
-    this.pane.fillStyle = "red";
-    this.pane.fillCircle(new Vector2f(10, 10), 5);
+    "use strict";
+    
+    console.log(this.pane);
+    // this.pane.fillStyle = "red";
+    // this.pane.fillCircle(new Vector2f(10, 10), 5);
 };
 
 function play_game() {
--- js/game/game.js
+++ js/game/game.js
@@ -1,6 +1,8 @@
 /*global Engine, console, Vector2f*/
 function Game() {
-    "use strict"; 
+    "use strict";
+    
+    Engine.call(this);
 }
 Game.prototype = new Engine();
 Game.prototype.constructor = Game;
@@ -8,11 +10,13 @@ Game.prototype.constructor = Game;
 Game.prototype.load = function () {
     "use strict";
     
+    console.log("Game loaded");
+    
     this.gamestate.position = new Vector2f(0, 0);
 };
 
 Game.prototype.update = function (deltaT) {
     "use strict";
     
-    throw "update(deltaT) not implemented";
+    console.log("update from Game");
 };
